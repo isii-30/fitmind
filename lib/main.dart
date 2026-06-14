297f3eea244337c913a7ddd5dfaba07fd5ee31a1
Isira <isiraadithya04@gmail.com>
2026-06-14 23:00:44 +0530

    Disable overscroll effect across the app

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,23 @@ import 'package:myapp/screens/splash_screen.dart';
 void main() {
   runApp(const MyApp());
 }
-
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Widget buildOverscrollIndicator(
+    BuildContext context,
+    Widget child,
+    ScrollableDetails details,
+  ) {
+    return child;
+  }
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scrollBehavior: MyCustomScrollBehavior(),
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
